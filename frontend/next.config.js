9907a8bf625dd785875dafc8dbe656372652d416
Maliek Davis <maliekjdavis24@gmail.com>
2023-03-19 23:52:50 -0400

    everything

---
+++ frontend/next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    publicRuntimeConfig: {
+        APP_NAME: 'Pearl Box',
+        API_DEVELOPMENT: 'http://localhost:8000/api',
+        API_PRODUCTION: 'http://pearlbox.co/api',
+        PRODUCTION: false,
+        DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+        DOMAIN_PRODUCTION: 'http://pearlbox.co'
+    },
+    images: {
+        domains: ['pearlbox.co'],
+      },
+};
