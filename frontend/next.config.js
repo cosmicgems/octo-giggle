18d80634b434c54d0ba67b4099d4f0ada8cb525e
Maliek Davis <maliekjdavis24@gmail.com>
2023-03-21 15:49:45 -0400

    sure

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@ module.exports = {
         APP_NAME: 'Pearl Box',
         API_DEVELOPMENT: 'http://localhost:8000/api',
         API_PRODUCTION: 'http://pearlbox.co/api',
-        PRODUCTION: false,
+        PRODUCTION: true,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
         DOMAIN_PRODUCTION: 'http://pearlbox.co',
         GOOGLE_CLIENT_ID: '412159963810-o58hvafvr25igt9dhbti3h51kai75um4.apps.googleusercontent.com'
