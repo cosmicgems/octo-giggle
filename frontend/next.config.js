9f6f339ffd20a617be8e6765d0cb25b8e9aa11a7
Maliek Davis <maliekjdavis24@gmail.com>
2023-03-21 19:27:07 -0400

    Masterpiece

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,10 +2,10 @@ module.exports = {
     publicRuntimeConfig: {
         APP_NAME: 'Pearl Box',
         API_DEVELOPMENT: 'http://localhost:8000/api',
-        API_PRODUCTION: 'http://pearlbox.co/api',
-        PRODUCTION: true,
+        API_PRODUCTION: 'https://pearlbox.co/api',
+        PRODUCTION: false,
         DOMAIN_DEVELOPMENT: 'http://localhost:3000',
-        DOMAIN_PRODUCTION: 'http://pearlbox.co',
+        DOMAIN_PRODUCTION: 'https://pearlbox.co',
         GOOGLE_CLIENT_ID: '412159963810-o58hvafvr25igt9dhbti3h51kai75um4.apps.googleusercontent.com'
     }
 };
